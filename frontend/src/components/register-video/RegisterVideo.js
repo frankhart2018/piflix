d4a9254249852d9d29f3c28690267ac78a1cefe5
frankhart2018 <choudhury.si@northeastern.edu>
2023-10-07 14:18:07 -0500

    Setup UI for registering video

--- frontend/src/components/register-video/RegisterVideo.js
+++ frontend/src/components/register-video/RegisterVideo.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const RegisterVideo = () => {
-  return <div>RegisterVideo</div>;
+  return (
+    <div className="container">
+      <p>
+        <input type="text" placeholder="Video name" />
+      </p>
+
+      <p>
+        <label for="video-file">Video file: </label>
+        <input type="file" id="video-file" />
+      </p>
+
+      <p>
+        <input type="button" value="Register video" />
+      </p>
+    </div>
+  );
 };
 
 export default RegisterVideo;
