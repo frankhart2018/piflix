e613b4c92b5ca1021b3157bdf4f9b2b18a285179
frankhart2018 <choudhury.si@northeastern.edu>
2023-10-07 23:16:59 -0500

    Hook video player with backend, to complete the basic version

--- frontend/src/components/show-video/ShowVideo.js
+++ frontend/src/components/show-video/ShowVideo.js
@@ -4,7 +4,15 @@ const ShowVideo = () => {
   const pathName = window.location.pathname;
   const videoId = pathName.split("/")[2];
 
-  return <div>ShowVideo({videoId})</div>;
+  const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000";
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <video id="videoPlayer" width="90%" controls autoplay muted="false">
+        <source src={`${API_BASE}/video/${videoId}`} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default ShowVideo;
