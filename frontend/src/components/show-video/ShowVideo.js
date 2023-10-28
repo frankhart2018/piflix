352308a23017c9ca6fb755c61004ead72aa5b102
frankhart2018 <choudhury.si@northeastern.edu>
2023-10-28 13:06:17 -0500

    Fix autoplay and unmuted in player

--- frontend/src/components/show-video/ShowVideo.js
+++ frontend/src/components/show-video/ShowVideo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const ShowVideo = () => {
   const pathName = window.location.pathname;
@@ -6,9 +6,23 @@ const ShowVideo = () => {
 
   const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000";
 
+  const videoRef = useRef();
+
+  useEffect(() => {
+    // TODO: Fetch this start time from DB
+    videoRef.current.currentTime = 40;
+  }, []);
+
   return (
     <div style={{ textAlign: "center" }}>
-      <video id="videoPlayer" width="90%" controls autoplay muted="false">
+      <video
+        id="videoPlayer"
+        width="90%"
+        controls
+        autoPlay
+        muted={false}
+        ref={videoRef}
+      >
         <source src={`${API_BASE}/video/${videoId}`} type="video/mp4" />
       </video>
     </div>
